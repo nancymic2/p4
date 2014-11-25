046073d23df83f35bce17525e83bc8daa14f8992
nancy michell <michell@fas.harvard.edu>
2014-11-24 21:12:56 -0500

    add migrations

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => 'scholarpaws.com',
 			'database'  => 'shoes',
 			'username'  => 'root',
 			'password'  => 'BDBpQgPk4N',
