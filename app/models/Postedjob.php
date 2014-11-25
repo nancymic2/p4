a92d07b6ce6bead1e294542114dd22970e6cbfef
nancy michell <michell@fas.harvard.edu>
2014-11-25 14:12:05 -0500

    stuff

--- app/models/Postedjob.php
+++ app/models/Postedjob.php
@@ -6,6 +6,7 @@ class PostedJob extends Eloquent
     public function user() {
         # job belongs to user
         # Define an inverse one-to-many relationship.
+        //naming issues here for caps in job
         return $this->belongsTo('User');
     }
 }
