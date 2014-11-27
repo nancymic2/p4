2b4e11c96ea9dffe95f096b99fda5cef5d648d36
nancy michell <michell@fas.harvard.edu>
2014-11-27 10:36:02 -0500

    styling

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -2,14 +2,24 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+
   <title>Career Trax</title>
 	<link rel="stylesheet" href="hmin.css">
 </head>
 <body>
-@yield ('home')
+@yield ('index')
 
 
 <!--/////////////////-->
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
 
