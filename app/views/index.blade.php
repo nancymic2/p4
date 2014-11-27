37f6bce11168105b5b07acdcc51947bd3def718c
nancy michell <michell@fas.harvard.edu>
2014-11-27 10:35:45 -0500

    styling

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,26 +2,33 @@
 	
 @extends('_master')
 
-@section ('home')
+@section ('index')
 
-
+<div class="container">
 <h3>welcome</h3>
 <h4>join career trax!</h4>
    
 
 
 
+
+
+
+
+
+
+
 <?php 
 
-echo '<a href="signup">sign up</a> <br>';
-echo '<a href="login">log in</a> <br>';
+echo '<a class="btn btn-success" href="signup">sign up</a> <br> <p>&nbsp;</p>';
+echo '<a class="btn btn-warning" href="login">log in</a> <br>';
 //echo '<a href="logout">log out</a> <br>';
 ?>
 
 
 
 
-
+</div>
 
 @stop
 
