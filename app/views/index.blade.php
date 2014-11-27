2bac269ac0b9f199647c59bc8046b7ede973dba6
nancy michell <michell@fas.harvard.edu>
2014-11-27 12:10:23 -0500

    styling

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -22,8 +22,8 @@
 
 <?php 
 
-echo '<a class="btn btn-success" href="signup">sign up</a> <br> <p>&nbsp;</p>';
-echo '<a class="btn btn-warning" href="login">log &nbsp;in</a> <br>';
+echo '<a class="btn btn-success" href="/signup">sign up</a> <br> <p>&nbsp;</p>';
+echo '<a class="btn btn-warning" href="/login">log &nbsp;in</a> <br>';
 //echo '<a href="logout">log out</a> <br>';
 ?>
 
