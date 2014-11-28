b89c1c3c8bccf8a7727794da23d6b1efacc6a54e
nancy michell <michell@fas.harvard.edu>
2014-11-28 08:24:43 -0500

    pword broken againagain again

--- app/database/migrations/2014_11_26_133112_create_password_reminders_table.php
+++ app/database/migrations/2014_11_26_133112_create_password_reminders_table.php
@@ -1,37 +1,33 @@
 <?php
-
-use Illuminate\Database\Schema\Blueprint;
+ 
 use Illuminate\Database\Migrations\Migration;
-
+ 
 class CreatePasswordRemindersTable extends Migration {
-
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('password_reminders', function(Blueprint $table)
-		{
-			$table->increments('id');//
-			$table->timestamps();//
-			$table->string('email')->index();
-			$table->string('token')->index();
-			
-			$table->integer('user_id')->unsigned();//
-			$table->foreign('user_id')->references('id')->on('users');//
-		});
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('password_reminders');
-	}
-
+ 
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('password_reminders', function($t)
+    {
+      $t->string('email');
+      $t->string('token');
+      $t->timestamp('created_at');
+    });
+  }
+ 
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::drop('password_reminders');
+  }
+ 
 }
+
