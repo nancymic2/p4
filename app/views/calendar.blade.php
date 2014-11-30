fefa07ac2734e07141c77af457a07bbf15015122
nancy michell <michell@fas.harvard.edu>
2014-11-29 19:09:17 -0500

    fixed calendar

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -5,14 +5,24 @@ $jobdate = Session::get('applybydate');
 
 $newdate=str_replace('-', '', $jobdate).'<br>'; 
 echo $newdate;
+$newdate2=$newdate+1;
 
-echo '<a href="http://www.google.com/calendar/event?action=TEMPLATE&text=microsoftapply&dates='.$newdate.'\/'.$newdate.'&details=&sprop=&location=home">add</a>';
-//echo input['applyby'];
-echo '<br>';
-echo '<a href="http://www.google.com/calendar/event?action=TEMPLATE&text=&dates='.$newdate.'%2F'.$newdate.'&details=&sprop=&location=home">add</a>';
-echo '<br>';
-echo '<a href="http://www.google.com/calendar/event?action=TEMPLATE&text=&dates='. $newdate. '/'. $newdate. '&details=&sprop= &location=home">add cal</a>';
 
-echo '<br>';
-$myurl=urlencode('http://www.google.com/calendar/event?action=TEMPLATE&text=&dates='. $newdate. '/'. $newdate. '&details=&sprop= &location=home');
-echo $myurl;
+/////////////////WORKING//////////////
+/*
+$newerurl= '<a href="http://www.google.com/calendar/event?action=TEMPLATE&text=&dates='
+.$newdate.'/'.$newdate.
+'&details=&sprop=&location=home">add to google calendar</a>';
+*/
+///////////////////////////////////
+
+$newerurl= '<a href="http://www.google.com/calendar/event?action=TEMPLATE&text=applyToJob&dates='
+.$newdate.'T090000'.'/'.$newdate.'T123000&details=&sprop=&location=Online">add to google calendar</a>';
+
+
+$newesturl=str_replace('%3Cbr%3E', '', $newerurl); 
+$newesturl2=str_replace('<br>', '', $newesturl); 
+
+
+echo '<br>'; 
+echo $newesturl2;
