08ad080cb387ac81d58485f382a7fad6dd27ca30
nancy michell <michell@fas.harvard.edu>
2014-12-03 20:35:18 -0500

    cleaanup

--- app/views/calendar.blade.php
+++ app/views/calendar.blade.php
@@ -15,9 +15,13 @@ $newerurl= '<a href="http://www.google.com/calendar/event?action=TEMPLATE&text=&
 '&details=&sprop=&location=home">add to google calendar</a>';
 */
 ///////////////////////////////////
+/*
+$newerurl= '<a href="http://www.google.com/calendar/event?action=TEMPLATE&text=applyToJob&dates='
+.$newdate.'T090000'.'/'.$newdate.'T123000&details=&sprop=&location=Online">add to google calendar</a>'; */
+
 
 $newerurl= '<a href="http://www.google.com/calendar/event?action=TEMPLATE&text=applyToJob&dates='
-.$newdate.'T090000'.'/'.$newdate.'T123000&details=&sprop=&location=Online">add to google calendar</a>';
+.$newdate.'T090000'.'/'.$newdate.'T123000&details=&sprop=&location=Online"'.' target="_blank">add to google calendar</a>';
 
 
 $newesturl=str_replace('%3Cbr%3E', '', $newerurl); 
@@ -26,3 +30,5 @@ $newesturl2=str_replace('<br>', '', $newesturl);
 
 echo '<br>'; 
 echo $newesturl2;
+echo '<br>'; 
+echo '<a href="/">Back</a>'; 
