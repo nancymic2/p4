2649b1722a81e6871dfde2f0034c65ad1de527f3
nancy michell <michell@fas.harvard.edu>
2014-12-06 13:45:11 -0500

    crap

--- public/deleteresume.php
+++ public/deleteresume.php
@@ -13,8 +13,51 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
+
+  <title>Career Trax</title>
+  <link rel="stylesheet" href="hmin.css">
 </head>
 <body>
+  <div class="container" style="margin:30px;">
+<img style="float:left" src="train2.jpg"> <h1>CareerTrax</h1> <h3>Get your job hunt on track.</h3>
+</div>
+</div>
+  <!--<div class="jumbotron">-->
+
+  <header class="navbar-inverse">
+    <div class="container">
+    <nav  role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">CareerTrax</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+        <li class="active"><a href="/resume">Add Resume<span class="sr-only">(current)</span></a></li>
+        <li><a href="/applications">Add Application</a></li>
+        <li><a href="/savedJobs">Add a Job</a></li>
+        <li><a href="/completedapps">Find Completed Apps</a></li>
+        <li><a href="/resumesearch">Find Your Resumes</a></li>
+        <li><a href="/jobstoapply">Find Saved Jobs</a></li>
+        <li><a href="/logout">Log Out</a></li>
+      
+      </ul>
+     
+      
+    </div><!-- /.navbar-collapse -->
+  <!--</div> container-fluid --> 
+</nav>
+</header>
+<body
 <?php
 
 $id = $_GET['id'];
@@ -46,9 +89,10 @@ if ($conn->query($sql) === TRUE) {
 }
 
 $conn->close();
-
+echo '<br><h3>Your Resume has been deleted</h3>';
 echo '<br><a class="btn btn-success" href="/">OK</a>';
 ?>
 
+</div>
 </body>
 </html>
