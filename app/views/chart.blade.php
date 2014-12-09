45f9e10cd9f38cd43b3a987445259a1027785419
nancy michell <michell@fas.harvard.edu>
2014-12-09 10:26:03 -0500

    calendar

--- app/views/chart.blade.php
+++ app/views/chart.blade.php
@@ -68,7 +68,8 @@ $newdata2=$salarycount[2];
   <link rel="stylesheet" href="hmin.css">
 </head>
 <body>
-
+  <br>
+<h3>This chart shows monthly salaries from jobs saved by all users of the site </h3>
 
 
 <!--Div that will hold the pie chart-->
