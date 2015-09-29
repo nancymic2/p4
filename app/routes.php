cedae704967795d67f2dd946350da5cf3337fb45
Nancy Michell <nancy.ann.michell@hp.com>
2015-09-29 07:32:10 -0400

    test no auth

--- app/routes.php
+++ app/routes.php
@@ -944,7 +944,7 @@ Route::get('/logout', function() {
 
 
 //below i changed before auth to before guest and test no auth on 9-29
-Route::get('resume', array('before' => 'guest', function()
+Route::get('resume', array('before' => 'auth', function()
          {
             return View::make('resume');
         }
