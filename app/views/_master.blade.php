e7ae29546d1c5c1d377596d4bee052106dc4b5ee
Nancy Michell <nancy.ann.michell@hp.com>
2015-10-11 12:12:04 -0400

    change label for saved jobs

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -53,7 +53,7 @@
 
         <!--changed add app to add job text link only 9-29   test to see if master.blade is used everywhere-->
         <li><a href="/applications">Add a Job</a></li>
-        <li><a href="/savedJobs">Add a Company</a></li>
+        <li><a href="/savedJobs">Add Something</a></li>
         <li><a href="/completedapps">Find Completed Apps</a></li>
         <li><a href="/resumesearch">Find Your Resumes</a></li>
         <li><a href="/jobstoapply">Find Saved Jobs</a></li>
