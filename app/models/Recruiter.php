8480ad7884188536d77cbc31f872423947419207
Nancy Michell <nancy.ann.michell@hp.com>
2015-10-11 10:16:47 -0400

    update models

---
+++ app/models/Recruiter.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Recruiter extends Eloquent
+{
+    public function user() {
+        # recruiter belongs to user
+        # Define an inverse one-to-many relationship.
+        return $this->belongsTo('User');
+    }
+
+
+        public function company() {
+        # recruiter belongs to company
+        # Define an inverse one-to-many relationship.
+        return $this->belongsTo('Company');
+    }
+
+}
