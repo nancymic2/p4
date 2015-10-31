bfaf2807a8ac079cc000dd59f53f53ecaba9855e
Nancy Michell <nancy.ann.michell@hp.com>
2015-10-31 09:14:50 -0400

    select in views for apps

--- app/views/applications.blade.php
+++ app/views/applications.blade.php
@@ -131,6 +131,9 @@ value="44"><br>
     company<br>
     {{ Form::text('company', '', array('class'=>'form-control'))}} 
 
+    {{ Form::select('category', $categories) }} <!-- 10/31/15 -->
+
+
 
     role<br>
     {{ Form::text('role', '', array('class'=>'form-control'))}} 
