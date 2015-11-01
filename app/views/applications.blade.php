6921898333f737f5aaf1297ddf4c2a396c956235
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-01 15:03:14 -0500

    add company hide

--- app/views/applications.blade.php
+++ app/views/applications.blade.php
@@ -168,8 +168,8 @@ company street<br>
 value="44"><br>-->
 <!-- remove rating -->
 <!--<a href="#" onclick='window.open("/companysmall", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=200, left=100, width=400, height=400, menubar=0, status=0, titlebar=0, toolbar=0, nomenubar=no, status=no, titlebar=no, toolbar=no" );'>Add a company</a>-->
-<a href="#" onclick='document.getElementById("companyhide2").style.display="block"; document.getElementById("companyhideDB").style.display="none"; '>Add the company if you have not added previously</a>
-  
+<a href="#companies" onclick='document.getElementById("companyhide2").style.display="block"; document.getElementById("companyhideDB").style.display="none"; '>Add the company if you have not added previously</a>
+  <a name="companies">
 <br><a href="#" onclick='document.getElementById("companyhide2").style.display="none"; document.getElementById("companyhideDB").style.display="block"; '>Or associate with one of your existing companies</a>
 
    <!-- add company<br>
