b0962a2e5b93bf2dfb13d6d8efd16318f9cc9b5b
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-05 15:36:06 -0500

    try select for resumes by user

--- app/routes.php
+++ app/routes.php
@@ -1139,7 +1139,7 @@ Route::get('applications', array('before' => 'auth', function()
           $company_lists = Company::lists('company', 'id');  /// 10/31/
           
           //$resume_lists = Resume::lists('name', 'id');  /// 11/1/15
-          $resume_lists = Resume::lists('name', 'user_id')->where('user_id', '=', $user->id);
+          $resume_lists = Resume::lists('user_id', 'name')->where('user_id', '=', $user->id);
 
           //$resume_lists = Resume::where('user_id', '=', $user->id)->lists('name', 'user_id');
 
