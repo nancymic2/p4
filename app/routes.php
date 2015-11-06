69c82e0bc95d03715a2a2258301cbbe4e2383dab
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-06 09:07:02 -0500

    try select for resumes by user

--- app/routes.php
+++ app/routes.php
@@ -1136,23 +1136,13 @@ Route::get('applications', array('before' => 'auth', function()
           //return view applications plus the array
           $user = Auth::user();
           //$resume_lists = Resume::where('user_id', '=', $user->id)->lists('name', 'user_id');
-          $company_lists = Company::lists('company', 'id');  /// 10/31/
+         // $company_lists = Company::lists('company', 'id');  /// 10/31/
           
-         // $resume_lists = Resume::lists('name', 'user_id'); /// 11/1/15
+          $company_lists = Company::where('user_id', '=', $user->id)->lists('company', 'user_id');
 
           $resume_lists = Resume::where('user_id', '=', $user->id)->lists('name', 'user_id');
 
-          //$resume_lists = Resume::where('votes', '>', 100)->get();
-          //$resume_lists = Resume::lists('user_id', 'name')->where('user_id', '=',Auth::user()->id)->get();
-
-
-          //$resume_lists = Resume::where('user_id', '=', $user->id)->lists('name', 'user_id');
-
-          //$resume_lists = Resume::where('user_id', '=', $user)->lists('name', 'user_id');
-
-
-          //$resume_lists = Resume::lists('name', $user);  /// 11/1/15
-         // $resume_lists->user()->associate(Auth::user()); /// 11/1/15  /////NEED TO ASSOCIATE WITH LOGGED IN USER FOR RESUMES AD COMPANIES SOMEHOW 11/1/15
+          /////NEED TO ASSOCIATE WITH LOGGED IN USER FOR RESUMES AD COMPANIES SOMEHOW 11/6/15
 
 
           return View::make('applications', array('company_lists' => $company_lists), array('resume_lists' => $resume_lists));  ///10/31/15
