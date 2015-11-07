daf34978e85753295378e49ccd767b33c7a01466
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-07 12:49:13 -0500

    just nav

--- app/views/applications.blade.php
+++ app/views/applications.blade.php
@@ -9,7 +9,11 @@
                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
                 <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
                 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
-<!-- datepicker was here -->
+                <script type="text/javascript">
+                    $(document).ready(function(){
+                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
+                    })
+                </script>
         </head>
 <div class="container">
     <div class="col-sm-6">
@@ -265,10 +269,6 @@ echo '<br><a class="btn btn-danger" href="/applications">Cancel</a>';
 
 </div>
 </div>
-                <script type="text/javascript">
-                    $(document).ready(function(){
-                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
-                    })
-                </script>
+
 @stop
 
