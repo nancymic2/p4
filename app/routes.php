f3fc0f9c9295b3a5739b5f39d9cefcf0ddebd348
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-07 07:38:55 -0500

    inout only in routes

--- app/routes.php
+++ app/routes.php
@@ -1137,10 +1137,10 @@ Route::get('applications', array('before' => 'auth', function()
           $user = Auth::user();
           
           /////NEED TO ASSOCIATE WITH LOGGED IN USER FOR RESUMES AD COMPANIES SOMEHOW 11/6/15
-
+$company_lists = Company::where('user_id', '=', $user->id)->get()->lists('company', 'user_id')->all();
 
           $resume_lists = DB::table('resumes')->where('user_id', $user->id)->lists('name');
-          $company_lists = DB::table('companies')->where('user_id', $user->id)->lists('company');
+          //$company_lists = DB::table('companies')->where('user_id', $user->id)->lists('company');
 
 
           return View::make('applications', array('company_lists' => $company_lists), array('resume_lists' => $resume_lists));  ///10/31/15
