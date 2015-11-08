774df4cdc834196a541cf1f6698278079ed0997b
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-08 08:56:38 -0500

    put html bak in routes

--- app/routes.php
+++ app/routes.php
@@ -18,66 +18,7 @@
 Route::get('account', array('before' => 'auth', function()
 
 {
-     $styles='<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-  <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-
-  <title>Career Trax</title>
-  <link rel="stylesheet" href="hmin.css">
-</head>
-<body>
-  <div class="container" style="margin:30px;">
-<img style="float:left" src="train2.jpg"> <h1>Get your job hunt on track.</h1>
-</div>
-</div>
-  <!--<div class="jumbotron">-->
-
-  <header class="navbar-inverse">
-    <div class="container">
-    <nav  role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="/">CareerTrax</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-        <li class="active"><a href="/resume">Add Resume<span class="sr-only">(current)</span></a></li>
-        <li><a href="/applications">Add a Job</a></li>
-        <li><a href="/savedJobs">Add Something</a></li>
-        <li><a href="/completedapps">Find Completed Apps</a></li>
-        <li><a href="/resumesearch">Find Your Resumes</a></li>
-        <li><a href="/jobstoapply">Find Saved Jobs</a></li>
-        <li><a href="/company">Add Company</a></li>
-        <li><a href="/logout">Log Out</a></li>
-      
-      </ul>
-     
-      
-    </div><!-- /.navbar-collapse -->
-  <!--</div> container-fluid --> 
-</nav>
-</header>';
+    
 $stylesend='</body></html>';
      
  $names = User::where('id', '=', Auth::user()->id)->get();
@@ -119,68 +60,9 @@ echo $stylesend;
 Route::get('changelast', array('before' => 'auth', function()
 
 {
-   $styles='<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-  <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-
-  <title>Career Trax</title>
-  <link rel="stylesheet" href="hmin.css">
-</head>
-<body>
-  <div class="container" style="margin:30px;">
-<img style="float:left" src="train2.jpg"> <h1>Get your job hunt on track.</h1>
-</div>
-</div>
-  <!--<div class="jumbotron">-->
-
-  <header class="navbar-inverse">
-    <div class="container">
-    <nav  role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="/">CareerTrax</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-        <li class="active"><a href="/resume">Add Resume<span class="sr-only">(current)</span></a></li>
-        <li><a href="/applications">Add a Job</a></li>
-        <li><a href="/savedJobs">Add Something</a></li>
-        <li><a href="/completedapps">Find Completed Apps</a></li>
-        <li><a href="/resumesearch">Find Your Resumes</a></li>
-        <li><a href="/jobstoapply">Find Saved Jobs</a></li>
-        <li><a href="/company">Add Company</a></li>
-        <li><a href="/logout">Log Out</a></li>
-      
-      </ul>
-     
-      
-    </div><!-- /.navbar-collapse -->
-  <!--</div> container-fluid --> 
-</nav>
-</header>';
+   
 $stylesend='</body></html>';
-echo $styles;
+include 'head.php';
 echo '<p>&nbsp;</p>';    
  $names = User::where('id', '=', Auth::user()->id)->get();
  
@@ -279,48 +161,7 @@ Route::get('/applicationupdate', function()
   return View::make('applicationupdate');
   
 });
-////////////////////////////////////
 
-///////////good//////////
-
-/*
-Route::get('password/remind', array(
-  'uses' => 'PasswordController@remind',
-  'as' => 'password.remind'
-));
-
-
-Route::post('password/remind', array(
-  'uses' => 'PasswordController@request',
-  'as' => 'password.request'
-));
-*/
-
-/*
-Route::get('password/reset', array(
-  'uses' => 'PasswordController@reset',
-  'as' => 'password.reset'
-));
-
-
-Route::post('password/reset', array(
-  'uses' => 'PasswordController@update',
-  'as' => 'password.update'
-));
-*/
-
-//////////good///////////
-/*Route::get('password/reset/{token}', array(
-  'uses' => 'PasswordController@reset',
-  'as' => 'password.reset'
-));
-
-//added $ in front ot token. undef error goes away but diff error dec 6 9am
-Route::post('password/reset', array(
-  'uses' => 'PasswordController@update',
-  'as' => 'password.update'
-)); */
-/////////////////////////////
 
 Route::get('password/reset/{token}', array(
   'uses' => 'RemindersController@getReset',
@@ -1366,3 +1207,46 @@ foreach($allsalaries as $salary) {
 
   } }
     );*/
+
+////////////////////////////////////
+
+///////////good//////////
+
+/*
+Route::get('password/remind', array(
+  'uses' => 'PasswordController@remind',
+  'as' => 'password.remind'
+));
+
+
+Route::post('password/remind', array(
+  'uses' => 'PasswordController@request',
+  'as' => 'password.request'
+));
+*/
+
+/*
+Route::get('password/reset', array(
+  'uses' => 'PasswordController@reset',
+  'as' => 'password.reset'
+));
+
+
+Route::post('password/reset', array(
+  'uses' => 'PasswordController@update',
+  'as' => 'password.update'
+));
+*/
+
+//////////good///////////
+/*Route::get('password/reset/{token}', array(
+  'uses' => 'PasswordController@reset',
+  'as' => 'password.reset'
+));
+
+//added $ in front ot token. undef error goes away but diff error dec 6 9am
+Route::post('password/reset', array(
+  'uses' => 'PasswordController@update',
+  'as' => 'password.update'
+)); */
+/////////////////////////////
