5ca341cafad85520e035d2139991fc17945fcb95
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-14 12:24:05 -0500

    change nav in head.php and master.blade

--- app/views/applications.blade.php
+++ app/views/applications.blade.php
@@ -79,7 +79,7 @@ value="44"><br>-->
 <div id="companyhideDB" style="display:none;">
 {{ Form::open(array('url' => '/applications')) }}
      <h4>Select existng company</h4>
-     {{ Form::select('company_id', $company_lists) }} <!-- this yieds the INDEX of the selected 11/6-->
+     {{ Form::select('company_id', $company_lists)->all() }} <!-- this yieds the INDEX of the selected 11/6-->
  <!-- 11/1/15  -->
        
       <h4>Select existing resume</h4>
