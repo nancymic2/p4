79a252ba5f299343f531da05e07f10ebcad0d5c1
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-14 08:45:17 -0500

    show hide company and resumess in add a job- apps.blade

--- app/views/applications.blade.php
+++ app/views/applications.blade.php
@@ -37,7 +37,7 @@ value="44"><br>-->
 <!-- remove rating -->
 <!--<a href="#" onclick='window.open("/companysmall", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=200, left=100, width=400, height=400, menubar=0, status=0, titlebar=0, toolbar=0, nomenubar=no, status=no, titlebar=no, toolbar=no" );'>Add a company</a>-->
 <a href="#companies" onclick='document.getElementById("companyhide2").style.display="block"; document.getElementById("companyhideDB").style.display="none"; document.getElementById("resumehideDB").style.display="none"; document.getElementById("resumehide").style.display="none";'>First: Add the company if you have not added previously</a>
-<br> <a href="#resume" onclick='document.getElementById("resumehide").style.display="block"; document.getElementById("resumehideDB").style.display="none"; '>Then: Add the resume used if you have not added previously</a>
+<br> <a href="#resume" onclick='document.getElementById("resumehide").style.display="block"; document.getElementById("companyhide2").style.display="none"; document.getElementById("resumehideDB").style.display="none"; '>Then: Add the resume used if you have not added previously</a>
  
 <br><a href="#companies" onclick='document.getElementById("companyhide2").style.display="none"; document.getElementById("resumehide").style.display="none"; document.getElementById("companyhideDB").style.display="block"; '>Then associate resume and company</a>
 
