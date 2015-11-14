21b8bee1f98fc6acd8a212ba97babaa026faaace
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-14 10:32:19 -0500

    expensereport reporting in routes all totals

--- app/routes.php
+++ app/routes.php
@@ -1082,23 +1082,23 @@ include 'head.php';
   </caption>
   <thead>
     <tr>  
-      <th width="82">Company</th>
-      <th width="82">Job</th>
-      <th width="82">Gas</th>
-      <th width="82">Air</th>
+      <th width="86">Company</th>
+      <th width="86">Job</th>
+      <th width="86">Gas</th>
+      <th width="86">Air</th>
       
-      <th width="82">Train</th>
+      <th width="86">Train</th>
 
-      <th width="82">Hotel</th>
-      <th width="82">Meals</th>
-      <th width="82">Postage</th>
-      <th width="82">Ink</th>
-      <th width="82">Paper</th>
-      <th width="82">Hardware</th>
-      <th width="82">Portfolio</th>
-      <th width="82">Phone</th>
-      <th width="82">Clothing</th>
-      <th width="82">Entertainment</th>
+      <th width="86">Hotel</th>
+      <th width="86">Meals</th>
+      <th width="86">Postage</th>
+      <th width="86">Ink</th>
+      <th width="86">Paper</th>
+      <th width="86">Hardware</th>
+      <th width="86">Portfolio</th>
+      <th width="86">Phone</th>
+      <th width="86">Clothing</th>
+      <th width="86">Entertainment</th>
 
 
 
@@ -1249,50 +1249,50 @@ foreach($expenses as $expense) {
 
     echo '<table border="1"><tr>';
 
-    echo '<td width="82">';
+    echo '<td width="86">';
     echo "Totals";
     echo '</td>';
 
-    echo '<td width="82">';
+    echo '<td width="86">';
     echo '</td>';
 
-    echo '<td width="82">';
+    echo '<td width="86">';
     echo $tgas;
     echo '</td>';
-    echo '<td width="82">';
+    echo '<td width="86">';
      echo $tair;
     echo '</td>';
-    echo '<td width="82">';
+    echo '<td width="86">';
     echo $ttrain;
     echo '</td>';
-    echo '<td width="82">';
+    echo '<td width="86">';
      echo $thotel;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $tmeals;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $tstamps;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $tink;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $tpaper;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $thardware;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $tportfolio;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $tphone;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $tclothing;
     echo '</td>';
-        echo '<td width="82">';
+        echo '<td width="86">';
          echo $tentertainment;
     echo '</td>';
 
