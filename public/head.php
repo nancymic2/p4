b64975505649e9def942bc3c33d83f267b0315fe
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-15 15:59:24 -0500

    ddown

--- public/head.php
+++ public/head.php
@@ -5,41 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-     <script src="//code.jquery.com/jquery.js"></script>
-      <script src="js/bootstrapmin.js"></script>
-      <script src="js/jscolor.js"></script>
 
   <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.0.1/css/bootstrap.min.css">
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
-<style type="text/css">
-.navbar-inverse { background-color: #222222}
-.navbar-inverse .navbar-nav>.active>a:hover,.navbar-inverse .navbar-nav>li>a:hover, .navbar-inverse .navbar-nav>li>a:focus { background-color: #}
-.navbar-inverse .navbar-nav>.active>a,.navbar-inverse .navbar-nav>.open>a,.navbar-inverse .navbar-nav>.open>a, .navbar-inverse .navbar-nav>.open>a:hover,.navbar-inverse .navbar-nav>.open>a, .navbar-inverse .navbar-nav>.open>a:hover, .navbar-inverse .navbar-nav>.open>a:focus { background-color: #080808}
-.dropdown-menu { background-color: #ffffff}
-.dropdown-menu>li>a:hover, .dropdown-menu>li>a:focus { background-color: #428bca}
-.navbar-inverse { background-image: none; }
-.dropdown-menu>li>a:hover, .dropdown-menu>li>a:focus { background-image: none; }
-.navbar-inverse { border-color: #080808}
-.navbar-inverse .navbar-brand { color: #999999}
-.navbar-inverse .navbar-brand:hover { color: #ffffff}
-.navbar-inverse .navbar-nav>li>a { color: #999999}
-.navbar-inverse .navbar-nav>li>a:hover, .navbar-inverse .navbar-nav>li>a:focus { color: #ffffff}
-.navbar-inverse .navbar-nav>.active>a,.navbar-inverse .navbar-nav>.open>a, .navbar-inverse .navbar-nav>.open>a:hover, .navbar-inverse .navbar-nav>.open>a:focus { color: #ffffff}
-.navbar-inverse .navbar-nav>.active>a:hover, .navbar-inverse .navbar-nav>.active>a:focus { color: #ffffff}
-.dropdown-menu>li>a { color: #333333}
-.dropdown-menu>li>a:hover, .dropdown-menu>li>a:focus { color: #ffffff}
-.navbar-inverse .navbar-nav>.dropdown>a .caret { border-top-color: #999999}
-.navbar-inverse .navbar-nav>.dropdown>a:hover .caret { border-top-color: #ffffff}
-.navbar-inverse .navbar-nav>.dropdown>a .caret { border-bottom-color: #999999}
-.navbar-inverse .navbar-nav>.dropdown>a:hover .caret { border-bottom-color: #ffffff}
-</style>
 
   <title>Career Trax</title>
   <link rel="stylesheet" href="hmin.css">
@@ -57,7 +30,7 @@
 <script type="text/javascript" >
 $(document).ready(function() 
 { 
-	$("#myTable").tablesorter(); 
+  $("#myTable").tablesorter(); 
 } 
 ); 
  
@@ -68,51 +41,40 @@ $(document).ready(function()
 <body>
   <div class="container" style="margin:30px;">
 <img style="float:left" src="train2.jpg"> <h1>Get your job hunt on track.</h1>
-
+</div>
+</div>
   <!--<div class="jumbotron">-->
 
-  <header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
-  <div class="container">
-    
-      <nav  role="navigation">
+  <header class="navbar-inverse">
+    <div class="container">
+    <nav  role="navigation">
   <div class="container-fluid">
-     <div class="navbar-header">   
-      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a href="/" class="navbar-brand">Home</a>
+      <a class="navbar-brand" href="/">CareerTrax</a>
     </div>
-    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-      <ul class="nav navbar-nav">
-        <li>
-        <a href="/applications">Add a Job, Company, Resume</a>
 
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Account<b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="user">Udate</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li><a href="#">Separated link</a></li>
-            <li><a href="/logout">Log Out</a></li>
-          </ul>
-        </li>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+  
+        <li><a href="/applications">Add a Job, Company, Resume</a></li>
         <li><a href="/expenses">Add Expenses</a></li>
         <li><a href="/expensereport">View Expenses</a></li>
         <li><a href="/completedapps">View Your Jobs</a></li>
         <li><a href="/resumesearch">View Your Resumes</a></li>
+        <li><a href="/logout">Log Out</a></li>
+      
       </ul>
-    </nav>
-  </div>
-</div>
-</div>
-</div>
+     
+      
+    </div><!-- /.navbar-collapse -->
+  <!--</div> container-fluid --> 
+</nav>
 </header>
-
-<hr style="height: 12px;
-    border: 0;
-    box-shadow: inset 0 12px 12px -12px rgba(0, 0, 0, 0.5);" >
