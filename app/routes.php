fec662593ab32e6959d60569115ea5c9eb316060
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-15 11:12:34 -0500

    revert company edit route

--- app/routes.php
+++ app/routes.php
@@ -1403,8 +1403,9 @@ Route::post('/applicationsrating',
     array(
         'before' => 'csrf', 
         function() {
+          $id =Input::get('id');
            //$company  = Company::find(1);  got to get the co id. take from url if need be cause ive had enough
-           $company  = Company::find(2);
+           $company  = Company::find($id);  ///works when id is hardcoded
             //$company->id   =Input::get('id');  //just added
             $company->website   = Input::get('website');
              $company->rating   = Input::get('rating');
