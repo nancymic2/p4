da30a65ead8cd5f83a73232f2dbbd781373c68ba
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-15 16:01:47 -0500

    ddown

--- public/head.php
+++ public/head.php
@@ -57,7 +57,7 @@ $(document).ready(function()
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">CareerTrax</a>
+      <a class="navbar-brand" href="/">CT</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
