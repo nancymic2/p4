0c85d38c015feb188bc06ab4547b4c7b3bcaab8d
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-15 07:02:38 -0500

    application edit in completedapps route table

--- app/routes.php
+++ app/routes.php
@@ -280,6 +280,7 @@ include 'head.php';
   <h3>Sort Your Applications by field</h3>
   <h4>Click Company to see or edit Company record</h4>
   <h4>Click Resume to see or edit Resume record (not hooked up yet)</h4>
+  <h4>Click Job name to see or edit Job record (not hooked up yet)</h4>
   </caption>
   <thead>
     <tr>  
