f8b8b8597b19802765c899dd318ec103e34920c3
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-16 07:46:12 -0500

    pass user to update

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -6,19 +6,20 @@
 <div class="container">
 	<div class="col-sm-6"
 <h2>&nbsp;</h2>
-<h2>Join CareerTrax! All user pages are behind the login screen.</h2>
+<h2>Welcome to CareerTrax!</h2>
 <p>test credentials: user test@test.com, password: testtest</p>
 <p>To see some data, try user: 4@5.com,  password: 123456
 <p>CareerTrax lets you keep track of 
 	<ul><li>your job appplications</li>
-		<li>jobs you plan to apply for</li>
+		<li>your job listings</li>
+		<li>your contacts</li>
 	    <li>your resumes</li>
+	    <li>your expenses</li>
 	    <li>important dates</li>
-	    <li>your candidate login info</li>
+	    <li>your candidate system login info</li>
 	</ul>    
 </p>
-	<p>No ads, no artilces, no clutter, and nothing to sell. Just a place to track your jub hunt activities and
-		get calendar reminders.</p>
+	<p>Organize your job hunt and keep your Career on Track.</p>
   <?php  echo '<a class="btn btn-primary" href="/charter">see salary chart</a> <p>&nbsp;</p>'; ?>
   <?php  echo '<a class="btn btn-primary" href="/charter">see company ratings</a> <p>&nbsp;</p>'; ?>
 
@@ -27,7 +28,7 @@
 <?php 
 
 $user = Auth::user();
-$id = Auth::user()->id;
+$id = Auth::user()->id;  //note this works bec there is only 1 logged-in user, not an array/object of
 echo '<a class="btn btn-success" href="/signup">sign up</a> <br> <p>&nbsp;</p>';
 echo '<a class="btn btn-primary" href="/login">&nbsp;log &nbsp;in&nbsp;</a> <p>&nbsp;</p>';
 //echo '<a class="btn btn-primary" href="/logout">&nbsp;log &nbsp;out</a> <br>';
