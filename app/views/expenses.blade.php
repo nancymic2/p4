2b206754b0b148df7989402d933e92b3c18fed58
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-16 10:52:37 -0500

    nav

--- app/views/expenses.blade.php
+++ app/views/expenses.blade.php
@@ -2,6 +2,19 @@
 
 @section ('expenses')
 
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+                        <script src="jquery.knob.js"></script>
+                <link href="/css/layout.css" rel="stylesheet">
+                <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+                <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
+                <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
+                <script type="text/javascript">
+                    $(document).ready(function(){
+                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
+                    })
+                </script>
+
 <div class="container">
     <div class="col-sm-6">
 <h2>&nbsp;</h2>
@@ -9,6 +22,9 @@
 
 {{ Form::open(array('url' => '/expenses')) }}
 <br>
+     <input class="date" name="exp_date" type="text" size="10"  id="exp_date" /><br>
+
+
   <h5>Add expense to one of your companies</h5>
 
      {{ Form::select('company_id', $company1_lists) }}
