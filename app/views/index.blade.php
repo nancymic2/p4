030bf2c05518a043c5e52dab28fc15a261d215ec
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-16 19:56:28 -0500

    rm acct nfo from index.blade

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -32,8 +32,8 @@ $id = Auth::user()->id;  //note this works bec there is only 1 logged-in user, n
 echo '<a class="btn btn-success" href="/signup">sign up</a> <br> <p>&nbsp;</p>';
 echo '<a class="btn btn-primary" href="/login">&nbsp;log &nbsp;in&nbsp;</a> <p>&nbsp;</p>';
 //echo '<a class="btn btn-primary" href="/logout">&nbsp;log &nbsp;out</a> <br>';
-echo '<a href="/account">get account details</a> <br>';
-echo '<a href="/changelast">change account</a> <br>';
+//echo '<a href="/account">get account details</a> <br>';
+//echo '<a href="/changelast">change account</a> <br>';
 echo '<a href="/password/remind">change password</a> <br>';
 
 echo '<a href="/user/edit/';
