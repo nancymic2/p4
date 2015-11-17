ae482a33508f7d3b5a9c333259e8f522eb8f8379
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-17 11:14:03 -0500

    add companies report add co links to edit

--- app/views/application-edit.blade.php
+++ app/views/application-edit.blade.php
@@ -20,7 +20,7 @@
     
     Note, if you want to associate with another company, you must <a href="#" onclick='document.getElementById("companies").style.display="block";'>view your companies</a> 
     and change this ID accordingly.<br>
-    You may see all of your <a href="/companyreport">full company records here.</a>
+    You may see all of your <a href="/companyreport">full company records here.</a><br>
     company ID<br>
     {{ Form::text('company')}} 
 <br>
