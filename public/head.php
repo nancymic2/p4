770c85bcca49e62416991685f877e7ec84add6d8
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-17 14:45:00 -0500

    your companies table header color move close

--- public/head.php
+++ public/head.php
@@ -72,27 +72,6 @@ $(document).ready(function()
   <!--<div class="jumbotron">-->
 
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Page 1-1</a></li>
-            <li><a href="#">Page 1-2</a></li>
-            <li><a href="#">Page 1-3</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
 
 
 
@@ -131,7 +110,13 @@ $(document).ready(function()
          <li><a href="/companyreport">Edit Companies</a></li>
         <li><a href="/expenses"><b>+</b>Expenses</a></li>
         <li><a href="/expensereport">Edit Expenses</a></li>
-       
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
         <?php 
 
 $user = Auth::user();
