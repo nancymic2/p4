e81f24f96ba200baa5ecf3279eec85955dc9c87a
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-17 16:42:49 -0500

    revert head.php

--- app/views/expenses.blade.php
+++ app/views/expenses.blade.php
@@ -2,7 +2,18 @@
 
 @section ('expenses')
 
-
+                <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+                        <script src="jquery.knob.js"></script>
+                <link href="/css/layout.css" rel="stylesheet">
+                <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+                <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
+                <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>-->
+                <script type="text/javascript">
+                    $(document).ready(function(){
+                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
+                    })
+                </script>
 
 <div class="container">
     <div class="col-sm-6">
