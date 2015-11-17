c280fb348a891cfc50eef3f6f8d5a4975b68b505
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-17 14:42:43 -0500

    your companies table header color move close

--- public/head.php
+++ public/head.php
@@ -71,6 +71,42 @@ $(document).ready(function()
 </div>
   <!--<div class="jumbotron">-->
 
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   <header class="navbar-inverse">
     <div class="container">
     <nav  role="navigation">
