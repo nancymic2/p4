733b4c8f80a89caabf0dbb5919101237b558fe6c
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-17 10:35:25 -0500

    add companies report add co links to edit

--- app/views/application-edit.blade.php
+++ app/views/application-edit.blade.php
@@ -18,7 +18,8 @@
 
 <br>
     
-    Note, if you want to associate with another company, you must <a href="/companyreport" target="_blank">view your companies</a> and change this ID accordingly.<br>
+    Note, if you want to associate with another company, you must <a href="" onclick='document.getElementById("companies").style.display="block";'>view your companies</a> 
+    and change this ID accordingly.<br>
     company ID<br>
     {{ Form::text('company')}} 
 <br>
@@ -40,5 +41,42 @@
 
 
 <br><a class="btn btn-danger" onclick="window.history.back()">Cancel</a>
+
+<div id="companies" style="display:none; position: absolute; top: 200px; left: 200px; z-index: 5;">
+
+<?php
+  $companies = Company::where('user_id', '=', Auth::user()->id)->get();
+    
+  echo '<div class="container">'; 
+  echo '<h2>Your Companies</h2>';
+
+  echo '<p>';
+  echo '<table border="2" cellpadding="4" style="width: 100%; table-layout: fixed;"> 
+    <tr>  
+      <td>Company name</td>
+      <td>Company ID</td>
+      
+      </tr>';
+//# loop through the Collection and access just the data
+foreach($companies as $company) {
+
+    echo '<tr>';
+    echo '<td>';
+ ////
+    echo $company['id'];
+    echo '</td>';
+    echo '<td>';
+    echo $company['company'];    
+    echo '</td>';
+    
+    echo '</tr>';
+
+    echo '</table><table border="2" cellpadding="4" style="width: 100%; table-layout: fixed;">';
+ 
+  }
+  ?>
+</div>
+
+
 <!-- note use email as the type vs text if you want validation -->
 @stop
