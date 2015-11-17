433574e184a7c5df6bf857a29d5977a956e0baa0
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-17 16:12:05 -0500

    your companies table header color move close

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -37,7 +37,7 @@ include 'head.php';
 @yield ('application-edit')
 <!--/////////////////-->
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
 
 </body>
 </html>
--- public/head.php
+++ public/head.php
@@ -8,7 +8,7 @@
       <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
                     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
                         <script src="jquery.knob.js"></script>
                 <link href="/css/layout.css" rel="stylesheet">
