3e8a0d9ff5e1ac59f624edd7e9ab50905c217582
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 09:02:33 -0500

    create nav in head.php

--- public/head.php
+++ public/head.php
@@ -169,18 +169,7 @@ $(document).ready(function()
 
 
 
-<!--<?php 
-
-$user = Auth::user();
-$id = Auth::user()->id;  //note this works bec there is only 1 logged-in user, not an array/object of
-
-
-echo '<li><a href="/user/edit/';
-echo $id;
-echo'">Edit acct info</a> </li>';
-//echo '<a href="logout">log out</a> <br>';
-?>
- -->      
+  
       
 
 </div>
