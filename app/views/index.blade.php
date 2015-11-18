0339b9a6eb0fa8e0c07b671c1dcf85c7a97bb356
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 09:15:52 -0500

    revert back 2 days head.php

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -32,14 +32,14 @@ $id = Auth::user()->id;  //note this works bec there is only 1 logged-in user, n
 echo '<a class="btn btn-success" href="/signup">sign up</a> <br> <p>&nbsp;</p>';
 echo '<a class="btn btn-primary" href="/login">&nbsp;log &nbsp;in&nbsp;</a> <p>&nbsp;</p>';
 //echo '<a class="btn btn-primary" href="/logout">&nbsp;log &nbsp;out</a> <br>';
-//echo '<a href="/account">get account details</a> <br>';
-//echo '<a href="/changelast">change account</a> <br>';
+echo '<a href="/account">get account details</a> <br>';
+echo '<a href="/changelast">change account</a> <br>';
 echo '<a href="/password/remind">change password</a> <br>';
 
 echo '<a href="/user/edit/';
 echo $id;
-//echo'">change acct info</a> <br>';
-//echo '<a href="logout">log out</a> <br>';
+echo'">change acct info</a> <br>';
+echo '<a href="logout">log out</a> <br>';
 ?>
 
 
