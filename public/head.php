8e9047bfb010ca8837e6316fbf3673ce110e8f34
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 10:03:44 -0500

    remove all refs to authed user in guest pages

--- public/head.php
+++ public/head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <link rel="stylesheet" href="css/MenuStyle.css">
+    <link rel="stylesheet" href="p4.scholarpaws.com/css/MenuStyle.css">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
