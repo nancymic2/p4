c3f03b23e9b558431ba91fb0619a0c5cf3b29a83
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 12:03:34 -0500

    rm buttons from HP

--- public/head.php
+++ public/head.php
@@ -27,10 +27,7 @@
                 
 
   <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 
 <!-- Latest compiled and minified JavaScript -->
 
