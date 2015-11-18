96ca6de28461d0a66ec35d3d88b32ab04bea3e9b
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 08:17:06 -0500

    create nav in head.php

--- public/head.php
+++ public/head.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <link rel="stylesheet" href="css/MenuStyle.css">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -74,7 +75,31 @@ $(document).ready(function()
   <!--<div class="jumbotron">-->
 
 
+<div id="wrapper">
 
+<nav id="nav">
+  <ul id="navigation">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Services &raquo;</a>
+      <ul>
+        <li><a href="#">Service 1</a></li>
+        <li><a href="#">Service 2</a></li>
+        <li><a href="#">Service 3 &raquo;</a>
+          <ul>
+            <li><a href="#">SubMenu 1</a></li>
+            <li><a href="#">SubMenu 2</a></li>
+            <li><a href="#">SubMenu 3</a></li>
+            <li><a href="#">SubMenu 4</a></li>
+          </ul>         
+        </li>         
+        <li><a href="#">Service 4</a></li>
+      </ul>
+    </li>
+  <li><a href="#" >Help</a></li>
+  </ul>
+</nav>
+
+</div><!--end wrapper-->
 
 
 
