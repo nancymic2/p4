8d7c61e2ed0b31a13ff9fc23d05b104a64b2c6dc
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 13:51:31 -0500

    new

--- public/head.php
+++ public/head.php
@@ -45,8 +45,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+
 
   </head>
   <body>
