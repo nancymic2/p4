cff576402cba57922614682a1e0c11f5a9d63109
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-19 08:38:05 -0500

    rm search in master.blade

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -88,12 +88,6 @@
       </ul>
 
 
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="/logout">Log Out</a></li>
         <li class="dropdown">
