798bec2dd2a382222e388f64cc04ef055d0c4d47
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-19 08:50:26 -0500

    login.blade add correct header

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,8 +4,21 @@
 
 @section ('login')
 
-<div class="container" style="width:500px; margin-left: -10px;">
-	<div class="col-sm-6">
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+                        <script src="jquery.knob.js"></script>
+                <link href="/css/layout.css" rel="stylesheet">
+                <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+                <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
+                <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
+            <script type="text/javascript">
+                    $(document).ready(function(){
+                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
+                    })
+                </script>
+
+<div class="container">
+    <div class="col-sm-6">
 <h2>&nbsp;</h2>
 <h2>Please log in</h2>
 
