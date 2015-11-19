372f8d05271ff2c4358f7a16be5b7b57d2df62a5
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-19 10:42:25 -0500

    creaate choose_resume route and view

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -155,7 +155,7 @@
 @yield ('account')
 @yield ('help')
 @yield ('calendar')
-@yield ('choose_resume')
+
 <!--/////////////////-->
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
