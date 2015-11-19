c87a1bfdfa9e35049baf8364335ae1f0b81e571b
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 22:57:52 -0500

    boots grid in master.blade

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -117,6 +117,7 @@
 				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
 			</p>
 		</div>
+
 		
 
 
@@ -146,8 +147,7 @@
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 <div class="col-md-2">.
-		</div>
-	</div>
+
 </div>
 
     <script src="js/jquery.min.js"></script>
