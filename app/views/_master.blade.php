e43fdb23cc1c385e33cbb4c62803731584256a95
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-19 09:31:31 -0500

    head and master add account ddown

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -98,6 +98,18 @@
           </ul>
         </li>
 
+                <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/account">Edit Account Info</a></li>
+            <li><a href="/completedapps">Resume</a></li>
+            <li><a href="/completedapps">Companies</a></li>
+            <li><a href="#">Contact</a></li>
+         
+          </ul>
+        </li>
+
+
       </ul>
 
 
