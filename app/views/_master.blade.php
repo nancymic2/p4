0e2a060e060674bc704d914b29f6dd77ab3d4ca5
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 20:15:45 -0500

    head back  3 days

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -38,8 +38,5 @@ include 'head.php';
 @yield ('account')
 <!--/////////////////-->
 
-<?php
-include 'close.php';
-?>
-
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
