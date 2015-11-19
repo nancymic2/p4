9b65df39af8349696f27e3815d1aa425e6bf350f
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-19 14:19:29 -0500

    de-actve li in master and head

--- public/head.php
+++ public/head.php
@@ -53,7 +53,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="/login">Log in<span class="sr-only">(current)</span></a></li>
+        <li><a href="/login">Log in</a></li>
         <li><a href="/help">Help</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Add <span class="caret"></span></a>
