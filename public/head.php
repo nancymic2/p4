8151ae87496b710c75062a2afa6187beb9d78d10
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 20:51:07 -0500

    head extra links

--- public/head.php
+++ public/head.php
@@ -63,7 +63,7 @@ $(document).ready(function()
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
+      <ul class="nav navbar-nav">
   
         <li><a href="/applications">+ Job, Co, Res</a></li>
         <li><a href="/expenses">Expenses</a></li>
