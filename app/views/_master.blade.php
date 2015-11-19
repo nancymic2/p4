d63ac8365f92ace0771a7cceb20dbb24849018d6
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-19 07:55:45 -0500

    added dropdown.js ref in master.blade

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -46,7 +46,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Home/a>
+      <a class="navbar-brand" href="/">Home</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -84,16 +84,25 @@
           </ul>
         </li>
 
+<li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Edit<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="/applications">Job</a></li>
+            <li><a href="/applications">Resume</a></li>
+            <li><a href="/applications">Company</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="/expenses">Expense</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
 
       </ul>
 
 
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+
       <ul class="nav navbar-nav navbar-right">
         <li><a href="/logout">Log Out</a></li>
         <li class="dropdown">
