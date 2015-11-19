584b15afe1366438e6d3d2297fd204c90576d9ac
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-19 08:24:37 -0500

    company.blade to match expeses.blade for nav

--- app/views/company.blade.php
+++ app/views/company.blade.php
@@ -2,8 +2,22 @@
 
 @section ('company')
 
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+                        <script src="jquery.knob.js"></script>
+                <link href="/css/layout.css" rel="stylesheet">
+                <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+                <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
+                <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
+            <script type="text/javascript">
+                    $(document).ready(function(){
+                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
+                    })
+                </script>
+
 <div class="container">
     <div class="col-sm-6">
+<h2>&nbsp;</h2>
 
 ////////////added 10-11-15
 
@@ -110,7 +124,7 @@
 
 <h2>&nbsp;</h2>
 <h2>Add a Company</h2>
-<p>Add a company</p>
+
 
 
 
