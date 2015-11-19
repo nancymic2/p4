22182d0d7921d8d999d935fe8dacc859eb850385
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 21:18:33 -0500

    head extra links

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -1,30 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<link rel="shortcut icon" href="http://p4.scholarpaws.com/favicon.ico" type="image/x-icon">
-<link rel="icon" href="http://p4.scholarpaws.com/favicon.ico" type="image/x-icon">
+
   <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="pw.css">
+
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 
-<script type="text/javascript" src="jquery.complexify.js"></script>
-<script type="text/javascript" src="rater.js"></script>
+<script type="text/javascript" src="http://p4.scholarpaws.com/js/jquery-latest.js"></script> 
+
+<script type="text/javascript" src="http://p4.scholarpaws.com/js/jquery.tablesorter.min.js"></script> 
 
   <title>Career Trax</title>
   <link rel="stylesheet" href="hmin.css">
+  <link rel="stylesheet" href="css/layout.css">
+
+
+<style type="text/css">
+#myTable > thead{
+  cursor: pointer;
+} 
+</style>
+</head>
+<body>
+<script type="text/javascript" >
+$(document).ready(function() 
+{ 
+  $("#myTable").tablesorter(); 
+} 
+); 
+ 
+
+
+</script>
 </head>
 <body>
   <div class="container" style="margin:30px;">
-<img style="float:left" src="http://p4.scholarpaws.com/train2.jpg"> <h1>Get your job hunt on track.</h1>
+<img style="float:left" src="http://p4.scholarpaws.com/train2.jpg">
 </div>
-
 </div>
   <!--<div class="jumbotron">-->
 
@@ -40,26 +58,23 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">CareerTrax Home</a>
+      <a class="navbar-brand" href="/">CT</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-        <!--<li class="active"><a href="/resume">Add Resume<span class="sr-only">(current)</span></a></li>-->
-
-        <!-- changed above to make resume not the active link. above it it alsways atve -->
-
-        <li><a href="/applications">Add a Job, Company, Resume</a></li>
-        <li><a href="/expenses">Add Expenses</a></li>
+      <ul class="nav navbar-nav">
+  
+        <li><a href="/applications">+ Job, Co, Res</a></li>
+        <li><a href="/expenses">Expenses</a></li>
         <li><a href="/expensereport">View Expenses</a></li>
-        <li><a href="/completedapps">View Your Jobs</a></li>
-        <li><a href="/resumesearch">View Your Resumes</a></li>
-
-
-        <!--WAS commented out 929-->
-
+        <li><a href="/completedapps">View Jobs</a></li>
+        <li><a href="/resumesearch">View Res</a></li>
+        <li><a href="/companyreport">View co</a></li>
+        <li><a href="/account">account</a></li>
         <li><a href="/logout">Log Out</a></li>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/charter">charts</a></li>
       
       </ul>
      
@@ -68,8 +83,7 @@
   <!--</div> container-fluid --> 
 </nav>
 </header>
-</div>
-
+</div></div></div><br><script src="js/jquery.min.js"></script><script src="js/bootstrap.min.js"></script><script src="js/scripts.js"></script>
 
 
 @yield ('index')
