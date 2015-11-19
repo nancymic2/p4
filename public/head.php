585e9b57ef4463665d2b4e84a31fff7abfcb6863
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-18 19:48:28 -0500

    fix datepicker

--- public/head.php
+++ public/head.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+                    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+                        <script src="jquery.knob.js"></script>
+                <link href="/css/layout.css" rel="stylesheet">
+                <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+                <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
+                <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
+                <script type="text/javascript">
+   
     <link rel="stylesheet" href="http://p4.scholarpaws.com/css/MenuStyle.css">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
