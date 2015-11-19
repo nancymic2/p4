9ffaa84de4a0ffbd004218cd0c3571424ede25f3
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-19 07:56:41 -0500

    added dropdown.js ref in master.blade

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -85,7 +85,7 @@
         </li>
 
 <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Edit<span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Add <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="/applications">Job</a></li>
             <li><a href="/applications">Resume</a></li>
@@ -102,7 +102,12 @@
       </ul>
 
 
-
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
       <ul class="nav navbar-nav navbar-right">
         <li><a href="/logout">Log Out</a></li>
         <li class="dropdown">
