7ffa8456219b258d00833db0b7a9d5ea022cb9a6
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-20 09:34:13 -0500

    try state in profile

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -1,6 +1,6 @@
 @extends('_master')
 
-@section ('resume')
+@section ('profile')
 
                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
                         <script src="jquery.knob.js"></script>
