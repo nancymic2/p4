c522d4c4a129b4810395028a04b61517908f16d2
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-20 08:58:36 -0500

    try state in profile

--- app/views/help.blade.php
+++ app/views/help.blade.php
@@ -20,15 +20,14 @@
 <h2>&nbsp;</h2>
 <h2>Help and FAQs</h2>
 
-<ul>
-    <li><b>Q</b> What does it cost to join CareerTrax? <br><b>A</b> CareerTrax is free!</li>
-     <li><b>Q</b> Will you sell my personal information?<br><b>A</b> No we wiill never sell or share your information with anyone.</li>
-     <li><b>Q</b> But you are showing salaries and degrees and other information on the site?<br><b>A</b> Yes but that information comes from all users and is aggregated and is not personally identifiable.</li>
-	 <li><b>Q</b> Why can't I add a profle?<br><b>A</b> If you are redirected to the edit profile page, that means there already exists a profile for your user ID.</li>
-	 <li><b>Q</b> Why can't I see dates when I open my expense report CSV file in Excel?<br><b>A</b> The CSV file is formatted properly. You must format the date column in excel to be date.</li>
-	
-	<li>4</li>
-</ul>
+
+    <p><b>Q</b> What does it cost to join CareerTrax? <br><b>A</b> CareerTrax is free!</p>
+     <p><b>Q</b> Will you sell my personal information?<br><b>A</b> No we wiill never sell or share your information with anyone.</p>
+     <p><b>Q</b> But you are showing salaries and degrees and other information on the site?<br><b>A</b> Yes but that information comes from all users and is aggregated and is not personally identifiable.</p>
+     <p><b>Q</b> Why can't I add a profle?<br><b>A</b> If you are redirected to the edit profile page, that means there already exists a profile for your user ID.</p>
+     <p><b>Q</b> Why can't I see dates when I open my expense report CSV file in Excel?<br><b>A</b> The CSV file is formatted properly. You must format the date column in excel to be date.</p>
+    
+
 
 </div>
 </div>
