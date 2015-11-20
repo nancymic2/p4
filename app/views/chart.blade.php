a2ae1ebe5aa2f32b7d1614c530a96d0a9ec54ae6
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-20 11:25:21 -0500

    add head sripts to chart.blade.php

--- app/views/chart.blade.php
+++ app/views/chart.blade.php
@@ -2,6 +2,21 @@
 @extends('_master')
 
 @section ('chart')
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+                        <script src="jquery.knob.js"></script>
+                <link href="/css/layout.css" rel="stylesheet">
+                <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+                <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
+                <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
+                <script type="text/javascript">
+                    $(document).ready(function(){
+                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
+                    })
+                </script>
+
+<div class="container">
+    <div class="col-sm-6">
 <?php
 
 $newdata0=$salarycount[0];
@@ -12,22 +27,8 @@ $newdata2=$salarycount[2];
 
 ?>
 
-                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
-                        <script src="jquery.knob.js"></script>
-                <link href="/css/layout.css" rel="stylesheet">
-                <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
-                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
-                <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
-                <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
-            <script type="text/javascript">
-                    $(document).ready(function(){
-                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
-                    })
-                </script>
 
-<div class="container">
-    <div class="col-sm-6">
-<h2>&nbsp;</h2>
+
     <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script type="text/javascript">
