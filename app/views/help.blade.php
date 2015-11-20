5b39678c7b20332ccf7562af288f05c1d74c8bad
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-20 09:06:23 -0500

    try state in profile

--- app/views/help.blade.php
+++ app/views/help.blade.php
@@ -25,8 +25,10 @@
      <p><b>Q</b> Will you sell my personal information?<br><b>A</b> No we wiill never sell or share your information with anyone.</p>
      <p><b>Q</b> But you are showing salaries and degrees and other information on the site?<br><b>A</b> Yes but that information comes from all users and is aggregated and is not personally identifiable.</p>
      <p><b>Q</b> Why can't I add a profle?<br><b>A</b> If you are redirected to the edit profile page, that means there already exists a profile for your user ID.</p>
-     <p><b>Q</b> Why can't I see dates when I open my expense report CSV file in Excel?<br><b>A</b> The CSV file is formatted properly. You must format the date column in excel to be date.</p>
-    
+     <p><b>Q</b> Why can't I see dates when I open my expense report CSV file in Excel?<br><b>A</b> The CSV file is formatted
+      properly. You must format the date column in excel to be date.</p>
+
+     <p><b>Q</b> Can you email me my password?<b>A</b> No we can't. Its encrypted and will be of no use to you anyway. Please use the forgot password function.</p>
 
 
 </div>
