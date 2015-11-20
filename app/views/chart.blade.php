9046b7df7720b2583979245c97b4d7669155f0fe
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-20 11:27:24 -0500

    add head sripts to chart.blade.php

--- app/views/chart.blade.php
+++ app/views/chart.blade.php
@@ -9,12 +9,13 @@
                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
                 <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
                 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
-                <script type="text/javascript">
+
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/js/umd/dropdown.js" type="text/javascript"></script>
+                                <script type="text/javascript">
                     $(document).ready(function(){
                     $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
                     })
                 </script>
-
 <div class="container">
     <div class="col-sm-6">
 <?php
