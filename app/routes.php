052b41b1bfbd0145927dc0483a6d281c9f771117
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-20 08:30:58 -0500

    try state in profile

--- app/routes.php
+++ app/routes.php
@@ -1874,7 +1874,7 @@ Route::get('profile', array('before' => 'auth', function()
 
             $profiles = Profile::where('user_id', '=', Auth::user()->id)->get();
             foreach($profiles as $profile) {
-              if ($profie['user_id']=='user_id'){
+              if ($profile['user_id']=='user_id'){
                 return View::make('account');
               }
             }
