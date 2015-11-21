c833d33fae13dca34dae43c03fe54c650be95f99
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-21 12:31:30 -0500

    nav

--- app/views/applications.blade.php
+++ app/views/applications.blade.php
@@ -70,9 +70,7 @@ if (diva.style.display=="none") {
 <h3>Rate this company</h3><br>
  
 
-  <input type="radio" name="rating" value="1">1 &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="2">2
-  &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="3">3&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="4">4
-  &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="5">5
+  <input type="radio" name="rating" value="1">1 &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="2">2&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="3">3&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="4">4&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="5">5
 
   <br>
   
@@ -101,9 +99,8 @@ if (diva.style.display=="none") {
 {{ Form::open(array('url' => '/applications')) }}
 
    <h3>Rate this job prospect</h3><br>
-  <input type="radio" name="rating" value="1">1 &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="2">2
-  &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="3">3&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="4">4
-  &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="5">5
+  <input type="radio" name="rating" value="1">1 &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="2">2&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="3">3&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="4">4&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="5">5
+
 
   <br>
      <h4>Select existing company</h4>
