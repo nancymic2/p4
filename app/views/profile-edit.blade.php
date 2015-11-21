2f912d9c0a651079be707e02825834384377e5b3
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-21 07:39:15 -0500

    profile-edit.blade add all selects

--- app/views/profile-edit.blade.php
+++ app/views/profile-edit.blade.php
@@ -61,21 +61,138 @@
 <br>
 
     Concentration<br>
-    {{ Form::text('major')}} 
+<select name="major">
+    <option value="Actuarial Science">Actuarial Science</option>    
+    <option value="Anthropology">Anthropology</option>      
+    <option value="Art">Art</option>        
+    <option value="Biochemistry">Biochemistry</option>      
+    <option value="Biological Sciences">Biological Sciences</option>        
+    <option value="Chemistry">Chemistry</option>    
+    <option value="Chemical Engineering">Chemical Engineering</option>      
+    <option value="Computer Engineering">Computer Engineering</option>      
+    <option value="Computer Science">Computer Science</option>      
+    <option value="Electrical Engineering">Electrical Engineering</option>      
+    <option value="Classics">Classics</option>      
+    <option value="Communication">Communications</option>       
+    <option value="Earth Science">Earth Science</option>        
+    <option value="Economics">Economics</option>        
+    <option value="English">English</option>        
+    <option value="Environmental Studies">Environmental Studies</option>        
+    <option value="History">History</option>        
+    <option value="Mathematics">Mathematics</option>        
+    <option value="Pharmacology">Pharmacology</option>      
+    <option value="Philosophy"></option>        
+    <option value="Physics">Physics</option>        
+    <option value="Political Science">Political Science</option>    
+    <option value="Psychology">Psychology</option>  
+</select>
 <br>
 
     Years of exp<br>
-    {{ Form::text('tenure')}} 
-<br>
+<select name="tenure">
+    <option value="2">Under 2</option>
+    <option value="4">2-4</option>
+    <option value="8">5-8</option>
+    <option value="12">9-12</option>
+    <option value="18">12-18</option>
+    <option value="25">19-25</option>
+    <option value="99">Over 25</option>
+
+</select><br>
 
     industry<br>
-    {{ Form::text('industry')}} 
+<select name="industry">
+    <option value="Actuarial Science">Actuarial</option>    
+    <option value="Art">Art</option>        
+    <option value="med">Medical</option>        
+    <option value="Biological Sciences">Biology</option>        
+    <option value="Chemistry">Chemistry</option>    
+    <option value="Chemical Engineering">Nursing</option>       
+    <option value="Computer Engineering">Computer Engineering</option>      
+    <option value="Computer Science">Computer Science</option>      
+    <option value="Electrical Engineering">Electrical Engineering</option>      
+    <option value="Classics">Classics</option>      
+    <option value="Communication">Communications</option>       
+    <option value="Earth Science">Earth Science</option>        
+    <option value="Economics">Economics</option>        
+    <option value="English">English</option>        
+    <option value="Environmental Studies">Environmental Studies</option>        
+    <option value="History">History</option>        
+    <option value="Mathematics">Mathematics</option>        
+    <option value="Pharmacology">Pharmacology</option>      
+    <option value="Philosophy"></option>        
+    <option value="Physics">Physics</option>        
+    <option value="Political Science">Political Science</option>    
+    <option value="Psychology">Psychology</option>  
+</select>
 <br>
     salary<br>
-    {{ Form::text('salrange')}} 
+<select name="salrange">
+    <option value="34">Under 40,000</option>
+    <option value="46">40,000 - 60,000</option>
+    <option value="68">61,000 - 80,000</option>
+    <option value="810">81,000 - 100, 000</option>
+    <option value="1012">101,000 - 120,000</option>
+    <option value="1214">121,000 - 140,000</option>
+    <option value="1416">141,000 - 160,000</option>
+    <option value="1699">Over 161,000</option>
+
+</select>
 <br>
     state<br>
-    {{ Form::text('state')}} 
+<select name="state">
+    <option value="AL">Alabama</option>
+    <option value="AK">Alaska</option>
+    <option value="AZ">Arizona</option>
+    <option value="AR">Arkansas</option>
+    <option value="CA">California</option>
+    <option value="CO">Colorado</option>
+    <option value="CT">Connecticut</option>
+    <option value="DE">Delaware</option>
+    <option value="DC">District Of Columbia</option>
+    <option value="FL">Florida</option>
+    <option value="GA">Georgia</option>
+    <option value="HI">Hawaii</option>
+    <option value="ID">Idaho</option>
+    <option value="IL">Illinois</option>
+    <option value="IN">Indiana</option>
+    <option value="IA">Iowa</option>
+    <option value="KS">Kansas</option>
+    <option value="KY">Kentucky</option>
+    <option value="LA">Louisiana</option>
+    <option value="ME">Maine</option>
+    <option value="MD">Maryland</option>
+    <option value="MA">Massachusetts</option>
+    <option value="MI">Michigan</option>
+    <option value="MN">Minnesota</option>
+    <option value="MS">Mississippi</option>
+    <option value="MO">Missouri</option>
+    <option value="MT">Montana</option>
+    <option value="NE">Nebraska</option>
+    <option value="NV">Nevada</option>
+    <option value="NH">New Hampshire</option>
+    <option value="NJ">New Jersey</option>
+    <option value="NM">New Mexico</option>
+    <option value="NY">New York</option>
+    <option value="NC">North Carolina</option>
+    <option value="ND">North Dakota</option>
+    <option value="OH">Ohio</option>
+    <option value="OK">Oklahoma</option>
+    <option value="OR">Oregon</option>
+    <option value="PA">Pennsylvania</option>
+    <option value="RI">Rhode Island</option>
+    <option value="SC">South Carolina</option>
+    <option value="SD">South Dakota</option>
+    <option value="TN">Tennessee</option>
+    <option value="TX">Texas</option>
+    <option value="UT">Utah</option>
+    <option value="VT">Vermont</option>
+    <option value="VA">Virginia</option>
+    <option value="WA">Washington</option>
+    <option value="WV">West Virginia</option>
+    <option value="WI">Wisconsin</option>
+    <option value="WY">Wyoming</option>
+</select>
 <br>
 
 
