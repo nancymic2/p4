7c62f70a83daf25461130d2dda2128422dda26f2
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-21 14:49:23 -0500

    redir passwword post to login

--- app/routes.php
+++ app/routes.php
@@ -185,9 +185,7 @@ Route::get('password/remind', array(
 
 Route::post('password/remind', array(
   'uses' => 'RemindersController@postRemind',
-  'as' => 'password.request',  function() {
-            return Redirect::to('/');
-        }
+  'as' => 'password.request'
 ));
 
 /////////////////////////////////
