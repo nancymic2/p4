af8740ecaf003cec5ba75c3b1e4df071e05b3ff2
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-21 15:25:49 -0500

    move header ig

--- public/head.php
+++ public/head.php
@@ -30,8 +30,9 @@
     </script>
 </head>
 <body>
+  <img style="float:left; max-width: 130%;" class="img-responsive" src="http://p4.scholarpaws.com/train2.jpg">
 <div class="container" style="margin:30px;">
-<img style="float:left; max-width: 130%;" class="img-responsive" src="http://p4.scholarpaws.com/train2.jpg">
+
 </div>
 </div>
   <!--<div class="jumbotron">-->
