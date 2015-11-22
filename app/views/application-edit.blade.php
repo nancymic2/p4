42a646a8039f8cb5afc82ab3da8c0744abfe06c8
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-22 15:16:09 -0500

    job edit

--- app/views/application-edit.blade.php
+++ app/views/application-edit.blade.php
@@ -31,9 +31,9 @@
 
 <br>
     
-    If you want to associate this job with another company, you must <b><u><a href="#" onclick='document.getElementById("companies").style.display="block";'>view your companies</a></u></b> 
+    If you want to associate this job with another company, you must <b><u><a href="#divs" onclick='document.getElementById("companies").style.display="block";'>view your companies</a></u></b> 
     and change this ID accordingly.<br> 
-    You may see all of your <a href="/companyreport">full company records here.</a><br>
+    <a name="divs"></a>You may see all of your <a href="/companyreport">full company records here.</a><br>
     company ID<br>
     {{ Form::text('company')}} 
 <br>
