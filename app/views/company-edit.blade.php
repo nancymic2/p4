08ae3af2e0a68b75208c11f299f59cacd44d4c9f
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-22 13:06:35 -0500

    remove co name from company edit as it creates new record

--- app/views/company-edit.blade.php
+++ app/views/company-edit.blade.php
@@ -21,29 +21,7 @@
 <h2>&nbsp;</h2>
 <h3>View or Edit Company</h3>
   
-<b>Current state is:&nbsp;</b>
- <?php
-$myid=$company->id;
-$companies = Company::where('user_id', '=', Auth::user()->id)->get();
-$states[]="";
-$corpids[]="";
-$k=0;
-    foreach($companies as $company) {
-   
-   $states[$k] = $company->state;
-   $corpids[$k] = $company->id;
-   $key1 = array_search($myid, $corpids); // $key = 2;
 
-   //$key = array_search('green', $array); // $key = 2;
-    $mystate = $states[$key1];
- 
-    echo $mystate;
-   
-    $k++;
-   //echo $state[]
- }
-
-  ?>
   <br>
 
  {{ Form::model($company, array('route' => 'company.edit', $company->id)) }}    
