5ee95adacaea02840fce1242659996deed55f6fd
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-24 10:16:48 -0500

    google cal updates

--- app/views/applications.blade.php
+++ app/views/applications.blade.php
@@ -165,9 +165,9 @@ if (diva.style.display=="none") {
 <div id="companyhideDB" style="display:none;">
 {{ Form::open(array('url' => '/applications')) }}
 
-   <h3>Rate this job prospect</h3><br>
+  <!-- <h3>Rate this job prospect</h3><br>
   <input type="radio" name="rating" value="1">1 &nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="2">2&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="3">3&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="4">4&nbsp;&nbsp;&nbsp; <input type="radio" name="rating" value="5">5
-
+-->
 
   <br>
      <h4>Select existing company</h4>
@@ -202,7 +202,32 @@ if (diva.style.display=="none") {
     Interview Date <br>
      <input class="date" name="followupBy" type="text" size="10"  id="followupBy" /><br>
 
- 
+ <select name="rating">
+  <option value="0">12:00 am</option>
+  <option value="1">1:00 am</option>
+  <option value="2">2:00 am</option>
+  <option value="3">3:00 am</option>
+  <option value="4">4:00 am</option>
+  <option value="5">5:00 am</option>
+  <option value="6">6:00 am</option>
+  <option value="7">7:00 am</option>
+  <option value="8">8:00 am</option>
+  <option value="9">9:00 am</option>
+  <option value="10">10:00 am</option>
+  <option value="11">11:00 am</option>
+  <option value="12">12:00 pm</option>
+  <option value="13">1:00 pm</option>
+  <option value="14">2:00 pm</option>
+  <option value="15">3:00 pm</option>
+  <option value="16">4:00 pm</option>
+  <option value="17">5:00 pm</option>
+  <option value="18">6:00 pm</option>
+  <option value="19">7:00 pm</option>
+  <option value="20">8:00 pm</option>
+  <option value="21">9:00 pm</option>
+  <option value="22">10:00 pm</option>
+  <option value="23">11:00 pm</option>
+</select>
 
     <!--Resume used<br>
     {{ Form::text('resumeUsed', '', array('class'=>'form-control'))}} -->
