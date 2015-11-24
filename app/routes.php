dea1ce156fe5d1c2c1f143ab8fe699fbc706e42c
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-23 22:29:53 -0500

    flash message to resume blade

--- app/routes.php
+++ app/routes.php
@@ -716,7 +716,7 @@ Session::flash('success', 'Your resume has been added');
 
 
 
-           return Redirect::to('/resume')->withMessage('Welcome to CareerTrax!');
+           return Redirect::to('/applications')->withMessage('Welcome to CareerTrax!');
 
         }
     )
