08dde58fca3d87f9d1b5ba1bb94eed30d6ad6574
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-24 07:58:27 -0500

    fix calendar blade REVERT 2014

--- app/routes.php
+++ app/routes.php
@@ -774,7 +774,7 @@ Route::post('/company',
             }
             # Log the user in
            // Auth::login($user);
-Session::flash('success', 'Your company has been added');
+Session::flash('success', 'Your compan has been added');
            return Redirect::to('/applications')->with('flash_message', 'Welcome to CareerTrax!');
 
         }
@@ -1010,14 +1010,11 @@ Route::post('/applications',
 
            //return Redirect::to('/')->with('flash_message', 'Welcome to CareerTrax!');
 
-            $jobs = json_decode($application, TRUE);
-            $followupBy=$jobs['followupBy'];
-            Session::put('followupBy', $followupBy);
+            $times = json_decode($application, TRUE);
+            $applydate=$times['applyby'];
+            Session::put('applybydate', $applydate);
 
-           //echo $jobs['applyby'];
-            //echo $postedjob;
-
-            return View::make('calendar2');
+            return View::make('calendar');
 
         }
     )
