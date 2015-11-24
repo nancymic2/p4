78c11624c3fe91a2dcc13080db6357112757fcfd
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-24 12:08:51 -0500

    new url constructed routs and cal2. may revert

--- app/views/calendar2.blade.php
+++ app/views/calendar2.blade.php
@@ -3,7 +3,7 @@ $jobdate = Session::get('googlecal');
 
 
 //echo $jobdate;
-
+echo '<h4>Your job has been created.</h4>';
 echo '<br>';
 echo '<a href="';
 echo 'https://calendar.google.com/calendar/render?action=TEMPLATE&text=Interview&dates=';
@@ -13,9 +13,9 @@ echo '<h3>Add to Google Calendar</h3></a>';
 
 echo '<br>';
 
-echo '<h4>Your job has been created.</h4>';
+
 echo '<br>'; 
 //echo $googlecal;
 
 echo '<br>'; 
-echo '<br><a herf="#" onclick="window.history.back()">Cancel</a></div></div>';
+echo '<br><a href="#" onclick="window.history.back()">Cancel</a>';
