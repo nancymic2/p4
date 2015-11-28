9a0cb025a6e1849a78964852b8c590b69c28e3a0
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-28 10:29:47 -0500

    try var dump in routes for app edit vs return view

--- app/routes.php
+++ app/routes.php
@@ -1680,15 +1680,15 @@ Route::post('/applicationsrating',
           /////NEED TO ASSOCIATE WITH LOGGED IN USER FOR RESUMES AD COMPANIES SOMEHOW 11/6/15
       $company_lists = Company::where('user_id', '=', $user->id)->get()->lists('company', 'id');
       $resume_lists = Resume::where('user_id', '=', $user->id)->get()->lists('name', 'id');   //changed resume to name
-      
+
       $companies = Company::where('user_id', '=', Auth::user()->id)->get(); //just added
       //$currentintdate=$application['followupBy'];  /////
        Session::put('appid', $id); /////
        /////////////////
    
-        // return our view 
-        return View::make('application-edit', array('company_lists' => $company_lists), array('resume_lists' => $resume_lists)) // pulls app/views/application-edit.blade.php use company/edit/1 or other id number
-            ->with('application', Application::find($id));
+      return var_dump($resume_lists);  
+        //return View::make('application-edit', array('company_lists' => $company_lists), array('resume_lists' => $resume_lists)) // pulls app/views/application-edit.blade.php use company/edit/1 or other id number
+            //->with('application', Application::find($id));
     }));
 
 
