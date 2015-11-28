4fd8edd85c890942f77907fe4ebf8f18d01dc268
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-28 12:14:00 -0500

    forget resume update in app update

--- app/views/application-edit.blade.php
+++ app/views/application-edit.blade.php
@@ -3,38 +3,136 @@
 
 @section ('application-edit')
 
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+                        <script src="jquery.knob.js"></script>
+                <link href="/css/layout.css" rel="stylesheet">
+                <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
+                <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
+                <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
+            <script type="text/javascript">
+                    $(document).ready(function(){
+                    $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
+                    })
+                </script>
 
 <div class="container">
-<div class="col-sm-6">
+    <div class="col-sm-6">
 <h2>&nbsp;</h2>
+<h3>View or Edit job</h3>
 
+  
 
-<h3>View or Edit Appication</h3>
-<br>
+  <br>
 
  {{ Form::model($application, array('route' => 'application.edit', $application->id)) }}    
 
 
-    company Name<br>
-    {{ Form::text('company')}} 
-<br>
-    role<br>
-    {{ Form::text('role')}} 
-<br>
-    job name<br>
-    {{ Form::text('recnumber')}} 
-<br>
-    hire manager<br>
-    {{ Form::text('hiringMgr')}} 
+<!--  <br>          
+        {{ Form::label('website', 'Website') }}
+          <br>
+        {{ Form::text('website', null, ['class'=>'form-control']) }}
+  <br>
+        {{ Form::label('street', 'Street') }}
+          <br>
+        {{ Form::text('street', null, ['class'=>'form-control']) }} -->
+  <br>
+        {{ Form::label('city', 'City') }}
+          <br>
+        {{ Form::text('city', null, ['class'=>'form-control']) }}
+  <br>
+<!--State <br>
+<select name="state" class="form-control">
+    <option value=""></option>
+    <option value="AL">Alabama</option>
+    <option value="AK">Alaska</option>
+    <option value="AZ">Arizona</option>
+    <option value="AR">Arkansas</option>
+    <option value="CA">California</option>
+    <option value="CO">Colorado</option>
+    <option value="CT">Connecticut</option>
+    <option value="DE">Delaware</option>
+    <option value="DC">District Of Columbia</option>
+    <option value="FL">Florida</option>
+    <option value="GA">Georgia</option>
+    <option value="HI">Hawaii</option>
+    <option value="ID">Idaho</option>
+    <option value="IL">Illinois</option>
+    <option value="IN">Indiana</option>
+    <option value="IA">Iowa</option>
+    <option value="KS">Kansas</option>
+    <option value="KY">Kentucky</option>
+    <option value="LA">Louisiana</option>
+    <option value="ME">Maine</option>
+    <option value="MD">Maryland</option>
+    <option value="MA">Massachusetts</option>
+    <option value="MI">Michigan</option>
+    <option value="MN">Minnesota</option>
+    <option value="MS">Mississippi</option>
+    <option value="MO">Missouri</option>
+    <option value="MT">Montana</option>
+    <option value="NE">Nebraska</option>
+    <option value="NV">Nevada</option>
+    <option value="NH">New Hampshire</option>
+    <option value="NJ">New Jersey</option>
+    <option value="NM">New Mexico</option>
+    <option value="NY">New York</option>
+    <option value="NC">North Carolina</option>
+    <option value="ND">North Dakota</option>
+    <option value="OH">Ohio</option>
+    <option value="OK">Oklahoma</option>
+    <option value="OR">Oregon</option>
+    <option value="PA">Pennsylvania</option>
+    <option value="RI">Rhode Island</option>
+    <option value="SC">South Carolina</option>
+    <option value="SD">South Dakota</option>
+    <option value="TN">Tennessee</option>
+    <option value="TX">Texas</option>
+    <option value="UT">Utah</option>
+    <option value="VT">Vermont</option>
+    <option value="VA">Virginia</option>
+    <option value="WA">Washington</option>
+    <option value="WV">West Virginia</option>
+    <option value="WI">Wisconsin</option>
+    <option value="WY">Wyoming</option>
+</select>
+  <br>
 
-  <br><br>
+
+<br>
+        {{ Form::label('phone', 'Phone') }}
+          <br>
+        {{ Form::text('phone', null, ['class'=>'form-control']) }}
+  <br>
+        {{ Form::label('careersite', 'Careersite') }}
+          <br>
+        {{ Form::text('careersite', null, ['class'=>'form-control']) }}
+  <br>
+        {{ Form::label('rating', 'Rating') }}
+          <br>
+        {{ Form::text('rating', null, ['class'=>'form-control']) }}
+  <br>
+        {{ Form::label('username', 'Username') }}
+          <br>
+        {{ Form::text('username', null, ['class'=>'form-control']) }}
+  <br>    
+        {{ Form::label('pasword', 'Pasword') }}
+          <br>
+        {{ Form::text('pasword', null, ['class'=>'form-control']) }} -->
+<br>
+    
+<br>
  {{ Form::hidden('id', $application->id) }}  <!-- just added -->
-        {{ Form::submit('Update job') }}
-<br><br>
+        {{ Form::submit('Update Application') }}
+<br>
     {{ Form::close() }}
 
 
 
 <br><a class="btn btn-danger" onclick="window.history.back()">Cancel</a>
+
+</div>
+</div>
+
 <!-- note use email as the type vs text if you want validation -->
 @stop
