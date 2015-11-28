d7e0cae8b24981aac7e2eea9473ffabbad8f4925
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-28 10:03:20 -0500

    form styles in co edit

--- app/views/company_edit.blade.php
+++ app/views/company_edit.blade.php
@@ -10,22 +10,19 @@
 	
 		<h2>Update: {{ $company->company }}</h2>
 	
-		<div class='form-group'>
+		
 		name
 			{{ Form::text('company', null, ['class'=>'form-control']) }}
-		</div>
 		
-	
 		
-		<div class='form-group'>
-website
+		website
 			{{ Form::text('website', null, ['class'=>'form-control']) }}
-		</div>
 		
-		<div class='form-group'>
-street
+		
+		
+		street
 			{{ Form::text('street', null, ['class'=>'form-control']) }}
-		</div>
+		
 		
 		rating 1-7<br>
 			{{ Form::text('rating', null, ['class'=>'form-control']) }}
