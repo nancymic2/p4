9a651cf6db942d2779ed1fa75c3f3db5b1b82df3
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-28 17:01:11 -0500

    canvas chart. also divide all costs by 10

--- app/views/jobhuntcosts.blade.php
+++ app/views/jobhuntcosts.blade.php
@@ -102,7 +102,7 @@ foreach($expenses as $expense) {
 
 
 
-<canvas id="salaryed" width="800" height="600" />
+<canvas id="salaryed" width="800" height="500" />
 
 
 transportation, hotel, meals, supplies, hardware, portfolio, phone, clothing, entertainment
@@ -112,7 +112,7 @@ transportation, hotel, meals, supplies, hardware, portfolio, phone, clothing, en
 window.onload=function(){
           var canvas=document.getElementById('salaryed');
           var ctx=canvas.getContext('2d');
-          var scores=[<?php echo $tink/10; ?>, <?php echo $tgas/100; ?>, <?php echo $tclothing/10; ?>, <?php echo $tphone/10; ?>, <?php echo $tink/10; ?>, <?php echo $tpaper/10; ?>];
+          var scores=[<?php echo $tink/10; ?>, <?php echo $tgas/10; ?>, <?php echo $tclothing/10; ?>, <?php echo $tphone/10; ?>, <?php echo $tink/10; ?>, <?php echo $tpaper/10; ?>];
           var width=50;
           var currx=50;
           var base=200;
