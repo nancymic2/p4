8e5152f254ae8b12ec8a638b46c3e1d60cd1433f
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-29 17:40:42 -0500

    addeventlistenter for mouse move 17

--- app/views/jobhuntcosts.blade.php
+++ app/views/jobhuntcosts.blade.php
@@ -116,7 +116,7 @@ foreach($expenses as $expense) {
                                   } /////////////////////////////////////////////////////////////
                                       //alert('<?php echo $tink/$counter; ?>'); ////////////////////
                                         //alert(685-<?php echo $tair/$counter; ?>);
-                                        alert(parseInt((685-<?php echo $tair/$counter; ?>)));
+                                        //alert(parseInt((685-<?php echo $tair/$counter; ?>)));
                                   });
           var costs=[<?php echo $tink/$counter; ?>,<?php echo $tair/$counter; ?>, <?php echo $tmeals/$counter; ?>, <?php echo $thardware/$counter; ?>, <?php echo $tportfolio/$counter; ?>, <?php echo $tgas/$counter; ?>, <?php echo $tclothing/$counter; ?>, <?php echo $tentertainment/$counter; ?>, <?php echo $tphone/$counter; ?>, <?php echo $thotel/$counter; ?>, <?php echo $ttrain/$counter; ?>, <?php echo $tink/$counter; ?>, <?php echo $tpaper/$counter; ?>];
           var width=50;
