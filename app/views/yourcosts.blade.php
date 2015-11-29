81df22f68649e51687db5245b9261f6abb78146e
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-29 10:01:23 -0500

    add chart to exp report

--- app/views/yourcosts.blade.php
+++ app/views/yourcosts.blade.php
@@ -371,7 +371,7 @@ echo '<a href="http://help.webconnex.com/article/196-import-csv-files-into-quick
 
 ?>
 
-<canvas id="allexpense" width="1110" height="450" style="margin-left: 78px;"/>
+<canvas id="allexpense" width="1110" height="450" style="margin-left: 78px;"></canvas>
 
 transportation, hotel, meals, supplies, hardware, portfolio, phone, clothing, entertainment
 <br><a class="btn btn-success" onclick="window.history.back()">Cancel</a>
