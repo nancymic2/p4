f0bfec3babcd4d825541b6117b4eda027c3e0422
Nancy Michell <nancy.ann.michell@hp.com>
2015-11-29 07:25:41 -0500

    add chart to exp report

--- app/views/yourcosts.blade.php
+++ app/views/yourcosts.blade.php
@@ -373,7 +373,7 @@ transportation, hotel, meals, supplies, hardware, portfolio, phone, clothing, en
     window.onload=function(){
           var canvas=document.getElementById('allexpense');
           var ctx=canvas.getContext('2d');
-          var scores=[<?php echo $tgas; ?>, <?php echo $tair; ?>, <?php echo $ttrain; ?>, <?php echo $thotel; ?>, <?php echo $tmeals; ?>, <?php echo $tstamps; ?>, <?php echo $tink; ?>, <?php echo $tpaper; ?>, <?php echo $thardware; ?>, <?php echo $tportfolio; ?>, <?php echo $tphone; ?>, <?php echo $tclothing; ?>, <?php echo $tentertainment; ?>];
+          var scores=[<?php echo $tgas/10; ?>, <?php echo $tair/10; ?>, <?php echo $ttrain/10; ?>, <?php echo $thotel/10; ?>, <?php echo $tmeals/10; ?>, <?php echo $tstamps/10; ?>, <?php echo $tink/10; ?>, <?php echo $tpaper/10; ?>, <?php echo $thardware/10; ?>, <?php echo $tportfolio/10; ?>, <?php echo $tphone/10; ?>, <?php echo $tclothing/10; ?>, <?php echo $tentertainment/10; ?>];
           var width=50;
           var currx=50;
           var base=200;
