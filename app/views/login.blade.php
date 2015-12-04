4e1229a08e21d43227b95c2ff6a545f598c0b6e8
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-03 22:05:23 -0500

    login notice

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -27,7 +27,7 @@
                                     logalert=document.getElementById('alert');
                                     logalert.style.display="block";
                                     logalert.style.cursor="pointer";
-                                    logalert.innerHTML="You last logged in on: " + history +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;X";
+                                    logalert.innerHTML="You last logged in on: " + history +"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;X";
                                     logalert.addEventListener('click', function(){
                                                                 logalert.style.display="none";
                                                              });
