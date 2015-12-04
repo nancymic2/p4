73e194984002f41f555f30f572e9a88586b72c99
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-03 22:58:12 -0500

    login notice

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -55,7 +55,7 @@ function isValid(pw) {
 
 //var nonword='/\W/';
 //var nonnumb='/\D/';
-if (value.search(/\D/) || value.search(/\W/){
+if (value.search('a') || value.search(b){
   alert (value);
 //}
 //return /^\w+$/.test(str); 
