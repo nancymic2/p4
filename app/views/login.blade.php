cb7500a682b65e5dae0e76bc093284a08f46c471
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-03 22:35:41 -0500

    login notice

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -49,7 +49,12 @@
 <script>
 function isValid(pw) { 
     value=document.getElementById('password').value;
-alert (value);
+
+var nonword=\W;
+var nonnumb=\D;
+if (nonword.test(value) || nonnumb.test(value){
+    alert (value);
+}
 //return /^\w+$/.test(str); 
 }
 </script>
