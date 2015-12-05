af871f848f80098761a39313f2bbc894f178ebcc
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-05 14:50:19 -0500

    try to hash pw

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -20,25 +20,27 @@
 <div class="container">
 <!--     <div class="col-sm-6">  -->
 
+<div style="width: 100%"><h2>Welcome to CareerTrax!</h2>
+<p>test credentials: user test@test.com, password: testtest</p>
+<p>To see some data, try user: 4@5.com,  password: 123456
+<p>CareerTrax lets you keep track of 
+    <ul><li>your job appplications</li>
+        <li>your job listings</li>
+        <li>your contacts</li>
+        <li>your resumes</li>
+        <li>your expenses</li>
+        <li>important dates</li>
+        <li>your candidate system login info</li>
+    </ul>    
+</p>
+</div><br>
 <div style="background-color: red; margin: 40px; width: 150px; height: 150px; float: left;"> </div>
 <div style="background-color: red; margin: 40px; width: 150px; height: 150px; float: left;"></div>
     <div style="background-color: red; margin: 40px; width: 150px; height: 150px; float: left;"></div>
         <div style="background-color: red; margin: 40px; width: 150px; height: 150px; float: left;"></div>
             <div style="background-color: red; margin: 40px; width: 150px; height: 150px; float: left;"></div>
-<h2>&nbsp;</h2>
-<h2>Welcome to CareerTrax!</h2>
-<p>test credentials: user test@test.com, password: testtest</p>
-<p>To see some data, try user: 4@5.com,  password: 123456
-<p>CareerTrax lets you keep track of 
-	<ul><li>your job appplications</li>
-		<li>your job listings</li>
-		<li>your contacts</li>
-	    <li>your resumes</li>
-	    <li>your expenses</li>
-	    <li>important dates</li>
-	    <li>your candidate system login info</li>
-	</ul>    
-</p>
+
+
 
 
 </div>
