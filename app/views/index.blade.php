0cf357112b76777cb0741036cad6359f2bc7fbb8
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-05 16:51:05 -0500

    buttons on hp

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -65,7 +65,7 @@
                     color: #6C2DC7;
                     text-align: center;
                     border-radius: 5px 40px 5px;
-                        text-shadow: 2px 2px 8px yellow;
+                        text-shadow: 2px 2px 8px #FFFF00;
                 }
 
                 </style>
