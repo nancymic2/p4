b534ab2be97c422f9e3d162a59402219b799478b
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-05 15:38:59 -0500

    buttons on hp

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,37 +4,44 @@
 @section ('index')
 
                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
-                        <script src="jquery.knob.js"></script>
+                <script src="jquery.knob.js"></script>
                 <link href="/css/layout.css" rel="stylesheet">
                 <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" type="text/css" media="all" />
                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js" type="text/javascript"></script>
                 <script src="//code.jquery.com/jquery-1.9.1.js"></script> 
                 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js" type="text/javascript"></script>
-            <script type="text/javascript">
+                <script type="text/javascript">
                     $(document).ready(function(){
                     $('input.date').datepicker({dateFormat: 'yy-mm-dd'});
                     })
                 </script>
                 <script>
-     $(document).ready(function()
-      { 
+                     $(document).ready(function()
+                      { 
 
-          buttons=document.getElementsByClassName("clickdiv");
-          for(var i=0;i<buttons.length;i++){
+                          buttons=document.getElementsByClassName("clickdiv");
+                          for(var i=0;i<buttons.length;i++){
 
-              buttons[i].addEventListener('click', gothere, false);
-              buttons[i].style.cursor="pointer";
-          }
-      
-      } 
-      );
+                              buttons[i].addEventListener('click', gothere, false);
+                              buttons[i].style.cursor="pointer";
+                          }
+                      
+                      } 
+                      );
 
-     function gothere(){
+                     function gothere(){
 
-        this.style.backgroundColor="#5EFB6E";
-        window.location=this.dataset.link;
-     }
-      </script>
+                        this.style.backgroundColor="#5EFB6E";
+                        window.location=this.dataset.link;
+                     }
+                </script>
+                <style type="text/css">
+                .clickdiv {
+                    padding: 20px;
+                    font-weight: bold;
+                    font-size: 20px;
+                }
+                </style>
 
 <!--<div class="container" style="background-image: url('home.jpg'); margin-top: -40px; width: 100%; background-repeat: no-repeat;">-->
 <div class="container">
