479aafe1d71b2ff8effa119cd301991a98c423ea
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-06 13:44:37 -0500

    vid on hp

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -83,7 +83,7 @@
 </p>
 </div><br>
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/yWgx5a3Su8o" frameborder="0" allowfullscreen></iframe>
+<iframe width="853" height="480" src="https://www.youtube.com/embed/yWgx5a3Su8o" frameborder="0" allowfullscreen></iframe><br>
 <div class="clickdiv" data-link= "signup"; data-bgcolor="#C3B6D7" style="border: 1px solid gray; background-color: #C3B6D7; margin: 30px; width: 150px; height: 150px; float: left; box-shadow: 10px 10px 5px #888888;">Sign Up </div>
 <div class="clickdiv" data-link= "login"; data-bgcolor="#eeeeee" style="border: 1px solid gray; background-color: #eeeeee; margin: 30px; width: 150px; height: 150px; float: left; box-shadow: 10px 10px 5px #888888;">Log In</div>
 
