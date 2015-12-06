0268fd5f41976d12908f91b445d767dbca992d23
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-06 08:07:23 -0500

    fill text on chart

--- app/views/jobhuntcosts.blade.php
+++ app/views/jobhuntcosts.blade.php
@@ -208,14 +208,17 @@ foreach($expenses as $expense) {
           ctx.stroke();
           ctx.closePath();
 
+ctx.font = "30px Verdana";
+// Create gradient
+var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+gradient.addColorStop("0", "magenta");
+gradient.addColorStop("0.5", "blue");
+gradient.addColorStop("1.0", "red");
+// Fill with gradient
+ctx.fillStyle = gradient;
+ctx.fillText("Big smile!", 10, 90);
 
 
-
-var c=document.getElementById("allexpese");
-var ctx=c.getContext("2d");
-
-//ctx.font="20px Georgia";
-ctx.fillText("Hello World!",10,50);
     };
     
 </script>
