1a77a047a3754d56bf637272284bc78118451b86
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-07 17:14:45 -0500

    signup modal 1

--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -4,6 +4,12 @@
 
 @section ('signup')
 
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  
                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
                         <script src="jquery.knob.js"></script>
                 <link href="/css/layout.css" rel="stylesheet">
@@ -52,7 +58,7 @@ $('#myModal').modal('show');
 
 
 
-        
+
         field.value='';
         } 
     }
