558c1a0167f4dc46b98dca9883622430a196168a
Nancy Michell <nancy.ann.michell@hp.com>
2015-12-07 14:50:57 -0500

    lables for canvas chart commit 4

--- app/views/jobhuntcosts.blade.php
+++ app/views/jobhuntcosts.blade.php
@@ -153,7 +153,7 @@ foreach($expenses as $expense) {
               ctx.font="13px Arial";
               ctx.fillText(parseInt(costs[i]), currx+18, canvas.height-h);
               ctx.font="14px Arial";
-              ctx.fillText(labels[i], currx+18, canvas.height-20);
+              ctx.fillText(labels[i], currx+14, canvas.height-20);
 
                 ///////////////////just added
 
